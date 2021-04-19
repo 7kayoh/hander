8e4f61b5e06ba1099c89bcb91633cd9c9e982050
va1kio <40730127+va1kio@users.noreply.github.com>
2021-04-19 13:50:30 +0000

    add comments

--- src/server/configuration.lua
+++ src/server/configuration.lua
@@ -1,9 +1,9 @@
 local configuration = {}
 
-configuration.groupId = 1
-configuration.ranks = {}
-configuration.pattern = "([<>]?)(%d+)"
+configuration.groupId = 1 -- use number
+configuration.ranks = {} -- >rank, <rank, rank
+configuration.pattern = "([<>]?)(%d+)" -- to check pattern of ranks
 
-configuration.maxRetries = 3
+configuration.maxRetries = 3 -- keep it at a reasonable amount
 
 return configuration
